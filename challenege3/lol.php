93dffd684a9410db87820fdfe188fb73aae0a67b
SwayamRao <swayamrao581@gmail.com>
2025-01-10 16:16:16 +0530

    challeneg 3 cookies

--- challenege3/lol.php
+++ challenege3/lol.php
@@ -1,14 +1,30 @@
 <?php
 // Define the correct cookie value
-$correct_cookie = "cookie_value";
+$correct_cookie = "cookie_monster6";
 
 // Check if the cookie is set and has the correct value
 if (isset($_COOKIE['challenge_cookie']) && $_COOKIE['challenge_cookie'] === $correct_cookie) {
     echo '<p>Congratulations! Here is your flag:</p>';
-    echo '<pre>secuRIT{Harry_is_the_Ch0sen_one}</pre>';
+    echo '<pre>secuRIT{C00kies_CaN_B3_Dang3rous!}</pre>';
 } 
 else if (isset($_COOKIE['challenge_cookie']) && $_COOKIE['challenge_cookie'] === "secuRIT"){
     echo 'Welcome Harry, Wanna to bake some cookies?';
+}
+else if (isset($_COOKIE['challenge_cookie']) && $_COOKIE['challenge_cookie'] === "cookie_monster1")
+{
+    echo 'Wow you figured this out?? But do you have enough patience? Lets see hehe';
+}
+else if (isset($_COOKIE['challenge_cookie']) && $_COOKIE['challenge_cookie'] === "cookie_monster3")
+{
+    echo 'One step at a time! Right? ';
+}
+else if (isset($_COOKIE['challenge_cookie']) && $_COOKIE['challenge_cookie'] === "cookie_monster4")
+{
+    echo 'Great one more step ahead ? this is last i swear';
+}
+else if (isset($_COOKIE['challenge_cookie']) && $_COOKIE['challenge_cookie'] === "cookie_monster5")
+{
+    echo 'Hehe are you even on right path? ';
 }
 else {
     echo '<p>You are on the correct path Harry, just try more</p>';
---
+++ challenege3/test.php
@@ -0,0 +1,37 @@
+<?php
+// Set the cookie value to 'secuRIT' every time the page is loaded
+setcookie('challenge_cookie', 'secuRIT', time() + 3600, '/'); // Expires in 1 hour
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookie Challenge</title>
+    <style>
+        body {
+            background-image: url('3ef3dcca3865adbfa2765bbb06d8b640-447504504.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to the Harry. I am the new monst.. sorry teacher here </h1>
+    <p>So class what should we bake today ?</p>
+    <p>Who said i resemble Dolores Umbridge?? </p>
+    <!--Hehe no you might need to take hint cause you wont get everything in inspect everytime ;)--> 
+    <p>
+        <?php
+     
+        $file_path = 'lol.php';
+        if (file_exists($file_path)) {
+            include $file_path;
+        } else {
+            echo 'Error: Validation file not found!';
+        }
+        ?>
+    </p>
+</body>
+</html>
